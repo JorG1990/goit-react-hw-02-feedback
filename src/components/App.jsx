0584b7a73e3ec6b3a417194a89c928af5bc34428
JorG1990 <jorge  garcia>
2023-07-25 22:09:55 -0500

    creacion carpetas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+import React, { Component } from "react";
+import FeedBackOptions from "./FeedBackOptions/FeedBackOptions";
+import Section from "./Section/Section";
+import Statistics from "./Statistics/Statistics";
+import Notification from "./Notification/Notification";
+
+class App extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
+
+  handleFeedback = (feedback) => {
+    this.setState ((prevState) => ({
+      ...prevState,
+      [feedback]: prevState [feedback] +1
+    }));
+  };
+}
+
+export default App;
